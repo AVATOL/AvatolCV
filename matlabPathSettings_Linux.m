c66cc736082731ac9739f6a3d748fae5d35e94b1
Jed Irvine <jedirv@gmail.com>
2014-07-14 14:33:30 -0700

    added dataset path to path settings, BOGUS dataset sdd.xml

--- matlabPathSettings_Linux.m
+++ matlabPathSettings_Linux.m
@@ -1,5 +1,6 @@
 path(path,'/nfs/guille/tgd/users/irvine/matlabui')
 path(path,'/nfs/guille/tgd/users/irvine/matlabui/questionnaire')
 path(path,'/nfs/guille/tgd/users/irvine/matlabui/tutorial')
+path(path,'/nfs/guille/tgd/users/irvine/matlabui/dataset') 
 path(path,'/nfs/guille/tgd/users/irvine/matlabui/tests') 
 
