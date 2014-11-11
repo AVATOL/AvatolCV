0ba8b1c816025ccc84195b93797d47f996a087a6
Jed Irvine <jedirv@gmail.com>
2014-11-11 14:19:37 -0800

    path setup files in sync

--- pathSetupWindows.m
+++ pathSetupWindows.m
@@ -1,14 +1,22 @@
+% system
 path(path,'C:\avatol\git\avatol_cv') 
 path(path,'C:\avatol\git\avatol_cv\app') 
 path(path,'C:\avatol\git\avatol_cv\questionnaire') 
 path(path,'C:\avatol\git\avatol_cv\tests') 
 path(path,'C:\avatol\git\avatol_cv\tutorial') 
 path(path,'C:\avatol\git\avatol_cv\dataset') 
-path(path,'C:\avatol\git\avatol_cv\algorithms') 
+path(path,'C:\avatol\git\avatol_cv\algorithms')
+% nematocyst
 path(path,'C:\avatol\git\nematocyst\avatol_system')
 path(path,'C:\avatol\git\nematocyst\avatol_system\parse_input')
 path(path,'C:\avatol\git\nematocyst\avatol_system\postprocess')
 path(path,'C:\avatol\git\nematocyst\avatol_system\preprocess')
 path(path,'C:\avatol\git\nematocyst\avatol_system\utilities')
 path(path,'C:\avatol\git\nematocyst')
+% bat
 path(path,'C:\avatol\git\bat')
+path(path,'C:\avatol\git\bat\rpm')
+path(path,'C:\avatol\git\bat\siftflow')
+path(path,'C:\avatol\git\bat\ssvm')
+path(path,'C:\avatol\git\bat\util')
+path(path,'C:\avatol\git\bat\visualization')
