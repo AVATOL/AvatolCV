eda1565dd22f44b6a807e3e081939c152d4e8870
Jed Irvine <jedirv@gmail.com>
2015-03-09 14:00:48 -0700

    some utiltiy classes to help figure out the json structures for Maria's spec

---
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/Annotation.java
@@ -0,0 +1,13 @@
+package edu.oregonstate.eecs.iis.avatolcv;
+
+public class Annotation {
+    Point[] points;
+    
+    public void setPoints(Point[] points){
+    	this.points = points;
+    }
+    public Point[] getPoints(){
+    	return this.points;
+    }
+    
+}
---
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/Annotations.java
@@ -0,0 +1,12 @@
+package edu.oregonstate.eecs.iis.avatolcv;
+
+public class Annotations {
+    Annotation[] annotations;
+    
+    public void setAnnotations(Annotation[] annotations){
+    	this.annotations = annotations;
+    }
+    public Annotation[] getAnnotations(){
+    	return this.annotations;
+    }
+}
