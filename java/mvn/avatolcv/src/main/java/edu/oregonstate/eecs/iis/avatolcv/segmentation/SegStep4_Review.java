3a926f4788a617ad2c0ebc22e41f5fb390ce2eed
Jed Irvine <jedirv@gmail.com>
2015-04-22 17:37:32 -0700

    progress on SegStep4_Review - now view can load.  Still need to save.

--- java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/segmentation/SegStep4_Review.java
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/segmentation/SegStep4_Review.java
@@ -1,32 +1,56 @@
 package edu.oregonstate.eecs.iis.avatolcv.segmentation;
 
+import java.util.List;
+
 import edu.oregonstate.eecs.iis.avatolcv.core.AvatolCVException;
+import edu.oregonstate.eecs.iis.avatolcv.core.ImageInfo;
 import edu.oregonstate.eecs.iis.avatolcv.core.Step;
 import edu.oregonstate.eecs.iis.avatolcv.core.View;
 
 public class SegStep4_Review implements Step {
 	private View view = null;	
 	private SegmentationSessionData ssd = null;
+	private boolean userReviewComplete = false;
 	public SegStep4_Review(View view, SegmentationSessionData ssd){
 		this.ssd = ssd;
 		this.view = view;
 	}
+	public List<ImageInfo> getOriginalImages(){
+	    return this.ssd.getCandidateImages();
+	}
+	public String getImageStatus(String ID) throws SegmentationException {
+	    return this.ssd.getSegmentationImages().getStatusForImage(ID);
+	}
+	public ImageInfo getTrainingImage(String ID) throws SegmentationException {
+	    return this.ssd.getSegmentationImages().getTrainingImage(ID);
+	}
+	public ImageInfo getResultImage(String ID) throws SegmentationException{
+	    return this.ssd.getSegmentationImages().getResultImage(ID);
+	}
+	/*
+	 * LEFT OFF HERE 4/22/15
+	 * Use cases from this screen
+	 * - user is ok with results and wants to move on
+	 * - user wants to manually fix a few and move on
+	 * - user wants to manually fix a few and then re-run segmentation to see if it does better
+	 */
+	public void validateResultFile(ImageInfo ii){
+	    
+	}
 	@Override
 	public void consumeProvidedData() throws AvatolCVException {
-		// TODO Auto-generated method stub
-
+	    
+		userReviewComplete = true;
 	}
 
 	@Override
 	public boolean needsAnswering() {
-		// TODO Auto-generated method stub
-		return false;
+		return !userReviewComplete;
 	}
 
 	@Override
 	public View getView() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.view;
 	}
 
 }
--- java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/segmentation/SegmentationImages.java
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/segmentation/SegmentationImages.java
@@ -2,18 +2,21 @@ package edu.oregonstate.eecs.iis.avatolcv.segmentation;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 import edu.oregonstate.eecs.iis.avatolcv.core.ImageInfo;
 
 public class SegmentationImages {
-
+    public static final String TRAINING_IMAGE = "train";
+    public static final String TEST_IMAGE = "test";
 	private List<ImageInfo> trainingImages = null;
 	private List<ImageInfo> testImages = null;
 	private List<ImageInfo> resultImages = null; 
 	private String segTrainingImageDir = null;
 	private String segOutputImageDir = null;
 	private List<ImageInfo> candidateImages = null;
+	private Hashtable<String, String> imageStatusForId = null;
 	public SegmentationImages(String segTrainingImageDir, String segOutputImageDir, List<ImageInfo> candidateImages) throws SegmentationException {
 		this.segTrainingImageDir = segTrainingImageDir;
 		this.segOutputImageDir = segOutputImageDir;
@@ -27,7 +30,31 @@ public class SegmentationImages {
             f.mkdirs();
         }
 	}
+	public ImageInfo getTrainingImage(String ID) throws SegmentationException {
+        for (ImageInfo ii : trainingImages){
+            if (ii.getID().equals(ID)){
+                return ii;
+            }
+        }
+        throw new SegmentationException("ID " + ID + " is not a training image.");
+    }
+    public ImageInfo getResultImage(String ID) throws SegmentationException{
+        for (ImageInfo ii : resultImages){
+            if (ii.getID().equals(ID)){
+                return ii;
+            }
+        }
+        throw new SegmentationException("ID " + ID + " is not a result image.");
+    }
+    
+	public String getStatusForImage(String ID) throws SegmentationException{
+	    if (!imageStatusForId.containsKey(ID)){
+	        throw new SegmentationException("no image status known for ID " + ID);
+	    }
+	    return imageStatusForId.get(ID);
+	}
 	public void reload()  throws SegmentationException {
+	    imageStatusForId = new Hashtable<String, String>();
 		trainingImages = new ArrayList<ImageInfo>();
 		testImages = new ArrayList<ImageInfo>();
 		File segLabelDir = new File(segTrainingImageDir);
@@ -39,16 +66,19 @@ public class SegmentationImages {
 		 * to avoid having to know about potential suffixes on the root names, search just by root names
 		 */
 		for (ImageInfo ii : candidateImages){
+		    String ID = ii.getID();
 			String fileRootToLookFor  = ii.getFilename_IdName();
 			boolean match = false;
 			for (File f : labelFiles){
 				if (f.getName().startsWith(fileRootToLookFor)){
 					this.trainingImages.add(ii);
+					imageStatusForId.put(ID, TRAINING_IMAGE);
 					match = true;
 				}
 			}
 			if (!match){
 				this.testImages.add(ii);
+				imageStatusForId.put(ID, TEST_IMAGE);
 			}
 		}
 		
