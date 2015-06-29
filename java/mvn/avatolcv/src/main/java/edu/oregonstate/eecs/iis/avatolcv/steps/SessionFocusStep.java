e2e0694e5cfab1c49bddb80114774a730355f568
Jed Irvine <jedirv@gmail.com>
2015-06-29 10:51:50 -0700

    adding first two step package files - the porting has begun

---
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/steps/SessionFocusStep.java
@@ -0,0 +1,35 @@
+package edu.oregonstate.eecs.iis.avatolcv.steps;
+
+import edu.oregonstate.eecs.iis.avatolcv.core.AvatolCVException;
+import edu.oregonstate.eecs.iis.avatolcv.core.ScoringAlgorithms;
+import edu.oregonstate.eecs.iis.avatolcv.core.SessionData;
+import edu.oregonstate.eecs.iis.avatolcv.core.Step;
+
+public class SessionFocusStep implements Step {
+    private ScoringAlgorithms scoringAlgorithms = null;
+    private SessionData sessionData = null;
+    public SessionFocusStep(SessionData sessionData) throws AvatolCVException {
+        this.sessionData = sessionData;
+    }
+    public ScoringAlgorithms getScoringAlgorithms(){
+        return this.sessionData.getScoringAlgorithms();
+    }
+    @Override
+    public void init() throws AvatolCVException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void consumeProvidedData() throws AvatolCVException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getView() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
