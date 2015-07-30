cf5c01ec462ee1459f088c33ecc79106fa8853ae
Jed Irvine <jedirvine@10-249-144-242.wireless.oregonstate.edu>
2015-07-30 14:12:08 -0700

    last change to now obsolete matlab_script_invoker

--- matlabScriptInvoker.m
+++ matlabScriptInvoker.m
@@ -1,18 +1,33 @@
 function matlabScriptInvoker(varargin)
     argCount = length(varargin);
-    if argCount < 3
+    if argCount < 2
         fprintf('usage:  matlabScriptInvoke(dirContainingScript, scriptName, args...\n');
     else
         dirContainingScript = varargin{1};
+        %addpath(dirContainingScript);
+        
         varargin = varargin(2:length(varargin));
         scriptName = varargin{1};
         varargin = varargin(2:end);
         fprintf('argCount is %d\n', argCount);
-       
+        fprintf('dirContainingScript is %s\n', dirContainingScript);
+        fprintf('scriptName is %s\n', scriptName);
         
         theFunction = str2func(scriptName);
         newArgList = mat2cell(varargin, 1, ones(1, numel(varargin)));
-        theFunction(newArgList{:});
+        hello();
+        %theFunction(varargin{:}); <<< use this one once we get functions located
+        %theFunction();
+        %theFunction(newArgList{:});
+        %newArgList = cell2mat(varargin);
+        %theFunction(newArgList{:});
+        %
+        % ___try hardcoded reference to hello() with it compiled in
+        %
+        
+        %THe problem - MCR can only run compiled matlab files! need to compile everything we ship.
+        %THis means we have to support two modes - running with the MCR and running with matlab proper
+        %THey will need to put into the props file a flag for matlab proper, default will be runtime.
     end
 end
 
