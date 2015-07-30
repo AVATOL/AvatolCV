360da6f38664b0e4ccf1ab89534b541f4fec91b8
Jed Irvine <jedirvine@10-249-144-242.wireless.oregonstate.edu>
2015-07-30 15:03:07 -0700

    added stubs for missing methods on FileSystemDataSource

--- java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/core/FileSystemDataSource.java
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/core/FileSystemDataSource.java
@@ -1,5 +1,9 @@
 package edu.oregonstate.eecs.iis.avatolcv.core;
 
+import java.util.List;
+
+import edu.oregonstate.eecs.iis.avatolcv.generic.DatasetInfo;
+
 public class FileSystemDataSource implements DataSource {
 
     @Override
@@ -12,5 +16,72 @@ public class FileSystemDataSource implements DataSource {
     public boolean isAuthenticated() {
         return true;
     }
+
+	@Override
+	public List<DatasetInfo> getDatasets() throws AvatolCVException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void loadPrimaryMetadataForChosenDataset(ProgressPresenter pp, String processName) throws AvatolCVException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadRemainingMetadataForChosenDataset(ProgressPresenter pp, String processName)
+			throws AvatolCVException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDefaultUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDefaultPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setChosenDataset(DatasetInfo di) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ChoiceItem> getScoringConcernItems(ScoringAlgorithms sa) throws AvatolCVException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getInstructionsForScoringConcernScreen(ScoringAlgorithms sa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setChosenScoringConcerns(List<ChoiceItem> items) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setChosenScoringConcern(ChoiceItem item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getDatasetSummaryText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
