0401f8758fac8290006a474d73a907f0835e6355
Jed Irvine <jedirv@gmail.com>
2015-11-05 16:05:06 -0800

    final simplifications to YaoRotateTest.m

--- modules/orientation/yaoOrient/YaoRotateTest.m
+++ modules/orientation/yaoOrient/YaoRotateTest.m
@@ -1,41 +1,52 @@
 function YaoRotateTest
     path(path,'.');
     % ----------------------------- Orientation Module ----------------------
-    inputImagesDir = '/Users/jedirvine/av/avatol_cv/modules/segmentation/yaoSeg/segOutput';
+    %inputImagesDir = '/Users/jedirvine/av/avatol_cv/modules/segmentation/yaoSeg/segOutput';
     %inputImagesDir = 'C:\Users\collwe\Desktop\avatol_cv\modules\segmentation\yaoSeg\segOutput';
+    inputImagesDir = 'C:\avatol\git\avatol_cv\modules\segmentation\yaoSeg\segOutput';
     
-    testImagesFile = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/testImagesFullPath.txt';
+    %testImagesFile = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/testImagesFullPath.txt';
     %testImagesFile = 'C:\Users\collwe\Desktop\avatol_cv\modules\orientation\yaoOrient\testImagesFullPath.txt';
+    testImagesFile = 'C:\avatol\git\avatol_cv\modules\orientation\yaoOrient\testImagesFullPath.txt';
     
-    testImagesMaskFile = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/testMaskImagesFullPath.txt';
+    %testImagesMaskFile = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/testMaskImagesFullPath.txt';
     %testImagesMaskFile = 'C:\Users\collwe\Desktop\avatol_cv\modules\orientation\yaoOrient\testMaskImagesFullPath.txt';
+    testImagesMaskFile = 'C:\avatol\git\avatol_cv\modules\orientation\yaoOrient\testMaskImagesFullPath.txt';
     
     %rotationOutputDir = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/orientationOutput';
     %rotationOutputDir = 'C:\Users\collwe\Desktop\avatol_cv\modules\orientation\yaoOrient\rotationOutput';
      
-    orientationOutputDir = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/orientationOutput';
+    %orientationOutputDir = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/orientationOutput';
     %orientationOutputDir = 'C:\Users\collwe\Desktop\avatol_cv\modules\orientation\yaoOrient\orientationOutput';
+    orientationOutputDir = 'C:\avatol\git\avatol_cv\modules\orientation\yaoOrient\orientationOutput';
 
-    pathAlignmentShipped = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/alignmentShipped';
+    %pathAlignmentShipped = '/Users/jedirvine/av/avatol_cv/modules/orientation/yaoOrient/alignmentShipped';
     %pathAlignmentShipped = 'C:\Users\collwe\Desktop\avatol_cv\modules\orientation\yaoOrient\alignmentShipped';
+    pathAlignmentShipped = 'C:\avatol\git\avatol_cv\modules\orientation\yaoOrient\alignmentShipped';
    
-   
+    mkdir(orientationOutputDir);
+    currentDir = pwd();
+    cd(orientationOutputDir);
+    cd('..');
+    parentDir = pwd();
+    cd(currentDir);
+    rotationOutputDir = sprintf('%s%s%s', parentDir,filesep,'rotationOutput');
     % ___need a way to derive rotationOutputDir from orientationOutputDir as that's the only one that will be passed in
-    rotationOutDirname = 'rotationOutput';
-    if ispc
-        parts = strsplit(orientationOutputDir, '\');
-        parent = parts{end-1};
-        mkdir(parent, rotationOutDirname);
-        rotationOutputDir = sprintf('%s\%s',parent,rotationOutDirname);
-    else
-        parts = strsplit(orientationOutputDir, '/');
-        parent = parts{end-1};
-        mkdir(parent, rotationOutDirname);
-        rotationOutputDir = sprintf('%s/%s',parent,rotationOutDirname);
-    end
-    fprintf('rotationOutputDir is %s',rotationOutputDir);
+    %rotationOutDirname = 'rotationOutput';
+    %if ispc
+    %    parts = regex(orientationOutputDir, '_', 'split');
+    %    parent = parts{end-1};
+    %    mkdir(parent, rotationOutDirname);
+    %    rotationOutputDir = sprintf('%s\%s',parent,rotationOutDirname);
+    %else
+    %    parts = regex(orientationOutputDir, '_', 'split');
+    %    parent = parts{end-1};
+   %     mkdir(parent, rotationOutDirname);
+   %     rotationOutputDir = sprintf('%s/%s',parent,rotationOutDirname);
+   % end
+    fprintf('rotationOutputDir created as %s',rotationOutputDir);
     
- %   Yao_Rotation(inputImagesDir, testImagesFile, testImagesMaskFile,'_rotatedOrig', '_rotatedMask', rotationOutputDir);
+    Yao_Rotation(inputImagesDir, testImagesFile, testImagesMaskFile,'_rotatedOrig', '_rotatedMask', rotationOutputDir);
     
     % ----------------------------- Alignment Module ----------------------
     %pathLibsvm = 'G:\libsvm-3.18\libsvm-3.18'; % make sure you already installed and mexed LIBSVM/matlab
@@ -45,11 +56,11 @@ function YaoRotateTest
     %pathVlfeat = 'G:\vlfeat\vlfeat'; 
     pathVlfeat = '/Users/jedirvine/av/avatol_cv/modules/3rdParty/vlfeat/vlfeat-0.9.20'; 
     %run([pathVlfeat '\toolbox\vl_setup']) 
- %   run([pathVlfeat '/toolbox/vl_setup'])
+    run([pathVlfeat '/toolbox/vl_setup'])
     
    
     
- %   [test_predict, model] = Yao_alignment(pathAlignmentShipped, rotationOutputDir, '_rotatedOrig','_rotatedMask','_alignOrig';,'_alignMask',orientationOutputDir  );
+    [test_predict, model] = Yao_alignment(pathAlignmentShipped, rotationOutputDir, '_rotatedOrig','_rotatedMask','_alignOrig','_alignMask',orientationOutputDir  );
 
     
     
