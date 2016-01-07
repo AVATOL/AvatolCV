15cdaa1ae04f94d27fa0db03ae4b86a0ca79b67c
Jed Irvine <jedirv@gmail.com>
2016-01-07 12:49:58 -0800

    new suite of debug algs to cheatCopy data for the leaf project

---
+++ modules/scoring/leafScoreCheat/leafScoreCheatRunner.py
@@ -0,0 +1,22 @@
+import sys, os, os.path, imp
+cheatcopy = imp.load_source('cheatcopy', '../../python/cheatcopy.py')
+
+def main():
+    if (len(sys.argv) < 2):
+        usage()
+        exit()
+    run_config_path = sys.argv[1]
+    #print "path is {0}".format(run_config_path)
+    path_of_this_script = os.path.realpath(__file__)
+    #print "path of this script : {0}".format(path_of_this_script)
+    path_of_parent = os.path.dirname(path_of_this_script)
+    path_of_pregen_data = os.path.join(path_of_parent, 'preGenData')
+    cheatcopy.cheatcopy(run_config_path, path_of_pregen_data)
+    
+def usage():
+    print "usage:  python leafScoreCheatRunner.py  <path of runConfigFile>"
+    print ""
+    print "   where some_command can be..."
+
+if __name__ == "__main__":
+   main()
