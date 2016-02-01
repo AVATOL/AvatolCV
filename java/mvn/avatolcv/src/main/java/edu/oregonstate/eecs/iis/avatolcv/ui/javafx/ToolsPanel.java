dcb7ef95c03275c231b92ab970b0bf94e8eca39a
Jed Irvine <jedirv@gmail.com>
2016-01-31 21:30:44 -0800

    make the toolsPanel static

--- java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/ui/javafx/ToolsPanel.java
+++ java/mvn/avatolcv/src/main/java/edu/oregonstate/eecs/iis/avatolcv/ui/javafx/ToolsPanel.java
@@ -1,18 +1,16 @@
 package edu.oregonstate.eecs.iis.avatolcv.ui.javafx;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import edu.oregonstate.eecs.iis.avatolcv.AvatolCVException;
 import edu.oregonstate.eecs.iis.avatolcv.javafxui.AvatolCVJavaFX;
-import edu.oregonstate.eecs.iis.avatolcv.session.RunSummary;
-import edu.oregonstate.eecs.iis.avatolcv.ui.javafx.ResultsReview.RunChoiceChangeListener;
 
 public class ToolsPanel {
+	private static FXMLLoader loader = null;
+	private static Scene toolsScene = null;
 	private Stage mainWindow = null;
-    private Scene scene = null;
     private Scene originScene = null;
     private AvatolCVJavaFX mainScreen = null;
 	public ToolsPanel(AvatolCVJavaFX mainScreen, Stage mainWindow) throws AvatolCVException {
@@ -22,13 +20,14 @@ public class ToolsPanel {
     }
 	public void initUI() throws AvatolCVException {
         try {
-            FXMLLoader loader = new FXMLLoader(JavaFXStepSequencer.class.getResource("ToolsPanel.fxml"));
-            loader.setController(this);
-            Parent resultsReview = loader.load();
+        	if (null == loader){
+        		loader = new FXMLLoader(JavaFXStepSequencer.class.getResource("ToolsPanel.fxml"));
+                loader.setController(this);
+                Parent p = loader.load();
+                toolsScene = new Scene(p, AvatolCVJavaFX.MAIN_WINDOW_WIDTH, AvatolCVJavaFX.MAIN_WINDOW_HEIGHT);
+        	}
             this.originScene = this.mainWindow.getScene();
-            this.scene = new Scene(resultsReview, AvatolCVJavaFX.MAIN_WINDOW_WIDTH, AvatolCVJavaFX.MAIN_WINDOW_HEIGHT);
-            this.mainWindow.setScene(scene);
-            
+            this.mainWindow.setScene(toolsScene);
         }
         catch(Exception e){
             throw new AvatolCVException(e.getMessage(),e);
